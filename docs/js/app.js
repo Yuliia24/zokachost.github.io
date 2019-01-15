61039ff9d61a439afcab9b56e4b6c1aa99934398
Timothy <31729676+kolesnik-tim@users.noreply.github.com>
2019-01-15 08:53:23 +0200

    fix

--- docs/js/app.js
+++ docs/js/app.js
@@ -4,3 +4,13 @@ $('select').each(function(){
 $('select').change(function(){
 	$(this).siblings('p').text( $(this).children('option:selected').text() );
 });
+
+
+
+var swiper = new Swiper('.intro-swiper', {
+	
+	navigation: {
+		nextEl: '.swiper-intro-next',
+		prevEl: '.swiper-intro-prev',
+	},
+});
